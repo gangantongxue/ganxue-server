0f3bad417931cacf88ab848a8f22661dd42f318e
gangantongxue <gangantongxue@outlook.com>
2025-03-11 20:00:05 +0800

    [feat] forget password

--- router/auth_router.go
+++ router/auth_router.go
@@ -1,5 +1,7 @@
 package router
 
-func AuthRouter() {
+import "ganxue-server/handler"
 
+func AuthRouter() {
+	AuthGroup.POST("reset-password", handler.ResetPassword())
 }
--- router/open_router.go
+++ router/open_router.go
@@ -5,8 +5,8 @@ import "ganxue-server/handler"
 func OpenRouter() {
 	OpenGroup.POST("/sign-up", handler.SignUp())
 	OpenGroup.POST("/sign-in", handler.SignIn())
-	OpenGroup.POST("/reset-password", handler.ResetPassword())
 	OpenGroup.POST("/refresh", handler.Refresh())
+	OpenGroup.POST("/forget-password", handler.ForgetPassword())
 
 	OpenGroup.GET("/ver-code", handler.VerCode())
 	OpenGroup.GET("/delete-user", handler.DeleteUser())
